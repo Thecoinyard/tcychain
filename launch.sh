308e2b5397b2697bdad89727b6a8682b8f0e8539
abdul alim <shaikhalim4u@gmail.com>
2020-01-18 00:36:22 +0530

    cc

--- launch.sh
+++ launch.sh
@@ -1,1 +1,3 @@
-$cee97@127.0.0.1:30310'  --port  30303 --ipcdisable --syncmode full --rpc --rpccorsdomain "*" --rpcaddr '0.0.0.0' --rpcport 8545 --unlock 0xBfE2FaB2C815B2Cf5ef42304aDa643224BB7d67a --passwor$
+$cee97@127.0.0.1:30310'  --port  30303 --ipcdisable --syncmode full --rpc --rpccorsdomain "*" --rpcaddr '0.0.0.0' --rpcapi 'personal,db,eth,net,web3,txpool,miner' --rpcport 8545 --unlock 0xBfE2FaB2C815B2Cf5ef42304aDa643224BB7d67a --password password.txt --allow-insecure-unlock --mine
+
+
