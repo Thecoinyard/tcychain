a3b4fd55b0e8e7fc3f1f1617dcc98bf52a700540
abdul alim <shaikhalim4u@gmail.com>
2020-01-17 21:55:30 +0530

    launch

--- launch.sh
+++ launch.sh
@@ -1,4 +1,1 @@
-# Copy Paste this command in ....../privnw_poa/node1
-# Boot node should be running
-# This is the sealer node
-geth --networkid 252692 --mine --datadir "../tcychain"  --port  30303 --ipcdisable --syncmode full --rpc --rpccorsdomain "*" --rpcport 8501  --rpcapi 'personal,db,eth,net,web3,txpool,miner' --unlock 0x2299e9A8b7D5bc1AFD29c777909ee5d8146dBa6E --password password.txt --bootnodes 'enode://87f45d44c26743b786d97b5bc6e09d90d05c028e51132f0ae3b163a2afaf74099bd7a0209561a7c61a3010fac28b22dbfae410c8e031f208203595d7b7846b0c@127.0.0.1:0?discport=30310'  console
+geth --networkid 252692 --datadir "./tcychain" --bootnodes 'enode://43c8317c04964cba19800e84674452c947b82bdd542051c944e48a291089e364ed764ff06b3501aedda8e61a9bae7c3a72ab343fb6639a1577f3a30e9729ee47@127.0.0.1:0?discport=30310'  --port  30303 --ipcdisable --syncmode full --rpc --rpccorsdomain "*" --rpcport 8545 --unlock 032912627010921a7A587a6c1EA550B3Ae69Ae52 --password password.txt --allow-insecure-unlock --mine console
